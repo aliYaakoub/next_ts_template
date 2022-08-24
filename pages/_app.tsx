be4a5bda2487ad940f289277696f9829ce019dcc
aliYaakoub <ali.yaakoub.2021@gmail.com>
2022-08-24 22:35:34 +0300

    added a progress bar when switching routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
