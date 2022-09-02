98d4982a690ab0e0b2db427b7b764971754b3332
aliYaakoub <ali.yaakoub.2021@gmail.com>
2022-09-02 17:04:30 +0300

    improved modal generator

--- .internals/generators/modal/index.mjs
+++ .internals/generators/modal/index.mjs
@@ -8,19 +8,71 @@ export const modalGenerator = {
       name: 'componentName',
       message: 'What should it be called?',
     },
+    {
+      type: 'directory',
+      name: 'basePath',
+      message:
+        'Where do you want it to be created? (if you want it to be nested in another folder please create it before generating)',
+      basePath: `${modalsBaseGeneratorPath}`,
+    },
+    {
+      type: 'confirm',
+      name: 'wantLoadable',
+      default: false,
+      message: 'Do you want to load the component asynchronously?',
+    },
+    {
+      type: 'confirm',
+      name: 'wantStyles',
+      default: false,
+      message: 'Do you want a css module for this component?',
+    },
   ],
-  actions: () => {
-    const path = `${modalsBaseGeneratorPath}/{{properCase componentName}}.tsx`;
+  actions: (data) => {
+    let path = '';
+    let actions = [];
 
-    const actions = [
-      {
+    if (data.wantLoadable || data.wantStyles) {
+      path = `${modalsBaseGeneratorPath}/${data.basePath}/{{properCase componentName}}`;
+
+      actions.push({
+        type: 'add',
+        path: `${path}/index.tsx`,
+        templateFile: './modal/index.tsx.hbs',
+        abortOnFail: true,
+        skipIfExists: true,
+      });
+
+      if (data.wantStyles) {
+        actions.push({
+          type: 'add',
+          path: `${path}/{{properCase componentName}}.module.css`,
+          templateFile: './modal/style.module.css.hbs',
+          abortOnFail: true,
+          skipIfExists: true,
+        });
+      }
+
+      if (data.wantLoadable) {
+        actions.push({
+          type: 'add',
+          path: `${path}/Loadable.ts`,
+          templateFile: './modal/Loadable.ts.hbs',
+          abortOnFail: true,
+          skipIfExists: true,
+        });
+      }
+    } else {
+      path = `${modalsBaseGeneratorPath}/${data.basePath}/{{properCase componentName}}.tsx`;
+
+      actions.push({
         type: 'add',
         path: path,
         templateFile: './modal/index.tsx.hbs',
         abortOnFail: true,
         skipIfExists: true,
-      },
-    ];
+      });
+    }
 
     return actions;
   },
