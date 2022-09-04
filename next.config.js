b0434811d698ef88481dedfc6e064f8d205d4b96
aliYaakoub <ali.yaakoub.2021@gmail.com>
2022-09-04 23:07:19 +0300

    added @svgr/webpack for direct svg import support

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
