5b03fad835ead560b33b993e1a2a4331055d10ba
aliYaakoub <ali.yaakoub.2021@gmail.com>
2022-09-20 23:51:41 +0300

    changed fonts config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'serif', ...defaultTheme.fontFamily.sans],
+        serif: ['Roboto Serif', 'sans-serif', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
